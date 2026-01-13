9ecdbe7f7ede8d0a88dc07d2206b9c5b5cd30818
Zaid Sabbagh <sabbaghzaid88@gmail.com>
2026-01-13 11:05:39 +0300

    fix: fixed a typo in the rubik font line of code

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -17,7 +17,7 @@
 
         <!-- Font stylesheets with display=swap for non-blocking render -->
         <link href="https://fonts.bunny.net/css?family=inter:400,500,600,700&display=swap" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500,600;700&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;600;700&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
         @routes
