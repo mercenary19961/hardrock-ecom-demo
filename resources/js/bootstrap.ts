a9258f3a05c889d0301551fa3862f5b7ce30ba8b
Zaid Sabbagh <sabbaghzaid88@gmail.com>
2026-01-17 14:34:43 +0300

    fix: added the coupon seeder into the database seeder

--- resources/js/bootstrap.ts
+++ resources/js/bootstrap.ts
@@ -4,11 +4,3 @@ window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 window.axios.defaults.withCredentials = true;
 window.axios.defaults.withXSRFToken = true;
-
-// Get CSRF token from meta tag and add to axios headers
-const token = document.head.querySelector('meta[name="csrf-token"]');
-if (token) {
-    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = (token as HTMLMetaElement).content;
-} else {
-    console.error('CSRF token not found');
-}
