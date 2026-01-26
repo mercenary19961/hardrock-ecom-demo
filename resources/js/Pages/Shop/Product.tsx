8ef002882d17429915efbbed94a01ce6a70fe987
Zaid Sabbagh <sabbaghzaid88@gmail.com>
2026-01-26 12:40:49 +0300

    fix: fixed the white space below the footer

--- resources/js/Pages/Shop/Product.tsx
+++ resources/js/Pages/Shop/Product.tsx
@@ -272,7 +272,7 @@ function ProductContent({
         <>
             <Head title={productName} />
 
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-8 pb-16">
                 {/* Breadcrumbs */}
                 <nav className="flex items-center gap-2 text-sm text-gray-500 mb-6 overflow-hidden">
                     <Link
